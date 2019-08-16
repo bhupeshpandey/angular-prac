1cf3d4b5d8568e4ccbc01c5f84a7b4f9a62176f1
bhupeshpandey <netfriend_bhupesh@yahoo.com>
2019-08-15 20:36:43 -0500

    4 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {PrimeNgModule} from './primeng.module';
+import {MetricsComponent} from './metrics/metrics.component';
+import {HealthComponent} from './health/health.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, PrimeNgModule ],
+  declarations: [ AppComponent, HelloComponent, MetricsComponent, HealthComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
